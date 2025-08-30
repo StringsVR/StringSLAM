fcb884393a6691383cdafcf3e523342f360b58be
StringsVR <takahashisatou1@gmail.com>
2025-08-30 01:06:12 -0400

    Complete DescriptorMatcher

--- include/StringSLAM/Features/KeypointExtractor.hpp
+++ include/StringSLAM/Features/KeypointExtractor.hpp
@@ -11,9 +11,13 @@ namespace StringSLAM::Features
         KeypointExtractor(std::shared_ptr<OrbWrapper> orb_) : orb(orb_) {};
         ~KeypointExtractor() = default;
 
+        // Get Keypoints from ORB->compute()
         void getKeypoints(Frame &frame);
+
+        // Draw those keypoints to frame
         void drawKeypoints(Frame &frame, cv::Mat &out, cv::Scalar color);
 
+        // Create Pointer Function
         static std::shared_ptr<KeypointExtractor> create(std::shared_ptr<OrbWrapper> orb_) {
             return std::make_shared<KeypointExtractor>(orb_);
         };
